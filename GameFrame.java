1ecbb4b6e410fb756af4690201ef790cf9584cbf
augustinassirius <57543708+augustinassirius@users.noreply.github.com>
2025-11-13 11:03:32 +0000

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,23 @@
+import java.awt.Color;
+
+
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame{
+
+	GamePanel panel;
+	
+	GameFrame(){
+		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+        this.setBackground(Color.DARK_GRAY);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+        
+	}
+}
